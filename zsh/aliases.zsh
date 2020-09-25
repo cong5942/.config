258875fe4689cc0bd22c4057c26bab221887b3bf
cong5942 <58645599+cong5942@users.noreply.github.com>
2020-09-25 17:33:46 +0800

    feat: 设置Clash代理

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -8,3 +8,7 @@ alias ta='tmux a'
 alias tk='tmux kill-session -t'
 
 alias myzsh='zsh ~/.config/zsh/myzsh.sh'
+
+alias setproxy="export ALL_PROXY=socks5://127.0.0.1:7891"
+alias unsetproxy="unset ALL_PROXY"
+alias ip="curl 'http://ip-api.com/json/?lang=zh-CN'"
